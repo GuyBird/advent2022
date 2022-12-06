17a351dac55978b8bfcefde7528b8c2c8db2e9e8
Guy Bird <06gbird@gmail.com>
2022-12-06 08:20:00 +0300

    Add day 6 solutions

---
+++ day6/part1.pl
@@ -0,0 +1,24 @@
+use v5.10;
+use strict;
+use warnings;
+use List::MoreUtils qw(uniq);
+
+my @input = <>;
+map {chomp} @input;
+
+my @stream = split //, $input[0];
+my $score;
+my $markers = 4;
+
+for my $i ( ($markers - 1) .. (scalar @stream) - 1) {
+    my @seen;
+    map {push @seen, $stream[$i - $_]} (0 .. ($markers - 1));
+
+    if (scalar(uniq(@seen)) == $markers) {
+        $score = $i + 1;
+        last;
+    }
+
+}
+
+say $score;
